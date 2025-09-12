a3ee68cc50924448b9e55fd5908c1f8a9e25023d
Matthew Hanna <irtheman@gmail.com>
2025-09-11 23:57:40 -0400

    Added banner

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,10 +1,10 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // main menu toggle
   var toggleButton = document.getElementById("menu-toggle");
   var menu = document.getElementById("primary-nav");
 
   if (toggleButton && menu) {
-    toggleButton.addEventListener("click", function() {
+    toggleButton.addEventListener("click", function () {
       menu.classList.toggle("js-menu-is-open");
     });
   }
@@ -14,4 +14,48 @@ $(document).ready(function() {
 
   // add lightbox class to all image links
   $("a[href$='.jpg'], a[href$='.png'], a[href$='.gif']").attr("data-lity", "");
+
+  // banner
+  const header = document.querySelector("header");
+  if (header) {
+    header.innerHTML = `
+    <button class="mh-banner">Matthew's Official Website</button>
+    <div class="mh-banner-content">
+        <div class="mh-banner-content-center"><strong>Welcome to one of Matthew's official websites!</strong><br/></div>
+        <div class="mh-banner-content-filler-first">&nbsp;</div>
+        <div>
+            <strong>Matthew's Websites</strong><br/
+            <ul>
+                <li><a href="https://matthewhanna.com">Business Website</a></li>
+                <li><a href="https://matthewhanna.net">Resume Website</a></li>
+                <li><a href="https://blog.matthewhanna.net">Coding Blog</a></li>
+                <li><a href="https://matthewhanna.me">Personal Blog</a></li>
+            </ul>
+        </div>
+        <div>&nbsp;</div>
+        <div>
+            <strong>Managed Websites</strong><br/
+            <ul>
+                <li><a href="https://jhausman.ninja">Jessica Hausman</a></li>
+                <li><a href="https://msgdrinking.ninja">Drinking Giraffe Blog</a></li>
+            </ul>
+        </div>
+        <div class="mh-banner-content-filler-second">&nbsp;</div>
+        <div class="mh-banner-content-center">Feel free to explore and learn more about Matthew's work and interests!</div>
+    </div>
+`;
+
+    const coll = document.getElementsByClassName("mh-banner");
+    for (let i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+          content.style.maxHeight = null;
+        } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+        }
+      });
+    }
+  }
 });
